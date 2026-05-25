c7d02a9cefd9025fe30909dead1c68313b0def7b
kimik17 <f17kimik@gmail.com>
2026-05-26 08:17:16 +0900

    김형준 최종 수정

---
+++ Item/Item.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+
+class Character; // 전방 선언
+
+class Item {
+public:
+    virtual ~Item() {}
+
+    virtual std::string getName() = 0;                     // 순수 가상 함수 (이름 반환 강제)
+    virtual void use(Character* character) = 0;            // 순수 가상 함수 (사용 기능 강제)
+    virtual int getPrice() = 0;
+    virtual bool isConsumable() { return false; }
+};
+
+std::vector<Item*> GetShopItemList();
+
+bool compareByPrice(Item* a, Item* b);
